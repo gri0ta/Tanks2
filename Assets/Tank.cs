217154e89a56c442e4effb9dad63c31ab2e4c938
vik <39262485+ViktorKornilov@users.noreply.github.com>
2023-11-05 13:01:13 +0200

    Tank movement, 2 player controls

---
+++ Assets/Tank.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Tank : MonoBehaviour
+{
+    public float speed = 5;
+    public float rotateSpeed = 90;
+
+    public string verticalAxis;
+    public string horizontalAxis;
+    public KeyCode shootKey;
+
+    void Update()
+    {
+        var ver = Input.GetAxis(verticalAxis);
+        transform.position += transform.forward * speed * ver * Time.deltaTime;
+
+        var hor = Input.GetAxis(horizontalAxis);
+        transform.Rotate(0,rotateSpeed * Time.deltaTime * hor,0);
+
+        if (Input.GetKeyDown(shootKey))
+        {
+            print("Shoot !!!");
+        }
+    }
+}
