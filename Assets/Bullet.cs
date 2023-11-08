1867bd6e3c7102a41e28a8df884cff88c0a86978
Ignasuss <146214932+gri0ta@users.noreply.github.com>
2023-11-08 19:08:28 +0200

    Particle explosion

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public float speed = 20;
+    public GameObject particle;
+    public float particleCount;
 
     void Start()
     {
@@ -12,5 +14,22 @@ public class Bullet : MonoBehaviour
     void Update()
     {
         transform.position += transform.forward * speed * Time.deltaTime;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Boom")
+        {
+            Destroy(collision.gameObject);
+
+            for (int i = 0; i < particleCount; i++)
+            {
+                var offset = Random.insideUnitSphere;
+                Instantiate(particle, transform.position + offset, transform.rotation);
+            }
+            
+        }
+
+        Destroy(gameObject);
     }
 }
